6f2ce2dba1c74b1e21714c1d6611cde7e2d89180
dangthai121200 <dangthai121200@gmail.com>
2023-02-11 23:33:47 +0700

    Create Backend

---
+++ WatchMovieGN-backend/src/main/java/watch/movie/gn/WatchMovieGnBackendApplication.java
@@ -0,0 +1,13 @@
+package watch.movie.gn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchMovieGnBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchMovieGnBackendApplication.class, args);
+	}
+
+}
