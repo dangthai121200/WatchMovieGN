cd97b9cc6c432d7e24b1f568e3ca084dc12a6c7b
thai.dang <dangthai121200@gmail.com>
2023-06-12 15:03:38 +0700

    Done Factory Pattern"

---
+++ WatchMovieGN-DesignPattern/src/main/java/watch/movie/gn/creational/factorymethod/Client.java
@@ -0,0 +1,8 @@
+package watch.movie.gn.creational.factorymethod;
+
+public class Client {
+    public static void main(String[] args) {
+        Bank bank = BankFactory.getBank(BankType.TPBANK);
+        System.out.println(bank.getBankName()); // TPBank
+    }
+}
---
+++ WatchMovieGN-DesignPattern/src/main/java/watch/movie/gn/creational/factorymethod/TPBank.java
@@ -0,0 +1,8 @@
+package watch.movie.gn.creational.factorymethod;
+
+public class TPBank implements Bank{
+    @Override
+    public String getBankName() {
+        return "TPBank";
+    }
+}
