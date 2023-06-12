16e5d87229708da108654a7817222ffeeb4aac20
thai.dang <dangthai121200@gmail.com>
2023-06-12 14:54:19 +0700

    Done singleton pattern"

---
+++ WatchMovieGN-DesignPattern/src/main/java/watch/movie/gn/creational/singleton/EnumSingleton.java
@@ -0,0 +1,5 @@
+package watch.movie.gn.creational.singleton;
+
+public enum EnumSingleton {
+    INSTANCE;
+}
---
+++ WatchMovieGN-DesignPattern/src/main/java/watch/movie/gn/creational/singleton/StaticBlockSingleton.java
@@ -0,0 +1,21 @@
+package watch.movie.gn.creational.singleton;
+
+public class StaticBlockSingleton {
+    private static final StaticBlockSingleton INSTANCE;
+
+    private StaticBlockSingleton() {
+    }
+
+    // Static block initialization for exception handling
+    static {
+        try {
+            INSTANCE = new StaticBlockSingleton();
+        } catch (Exception e) {
+            throw new RuntimeException("Exception occured in creating singleton instance");
+        }
+    }
+
+    public static StaticBlockSingleton getInstance() {
+        return INSTANCE;
+    }
+}
