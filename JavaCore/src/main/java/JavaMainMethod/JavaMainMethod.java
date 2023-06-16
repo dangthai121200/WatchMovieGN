417d19bfba3e9b7aeb56008c53fd14796ec9a6eb
thai.dang <dangthai121200@gmail.com>
2023-06-16 15:05:13 +0700

    Management memory"

---
+++ JavaCore/src/main/java/JavaMainMethod/JavaMainMethod.java
@@ -0,0 +1,13 @@
+package JavaMainMethod;
+
+public class JavaMainMethod {
+
+    static                  //static block
+    {
+        System.out.println("Static block");
+    }
+    public static void main(String[] args) {
+        System.out.println("Static method");
+    }
+
+}
