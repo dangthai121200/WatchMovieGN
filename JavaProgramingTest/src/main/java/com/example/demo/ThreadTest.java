cc4b0c2b86ae1be7f325008027747e693c8e1d88
DESKTOP-QS8C285\Thai Dang <dangthai121200@gmail.com>
2023-07-09 23:35:34 +0700

    Thread xmind: In-process

--- JavaProgramingTest/src/main/java/com/example/demo/ThreadTest.java
+++ JavaProgramingTest/src/main/java/com/example/demo/ThreadTest.java
@@ -1,38 +1,60 @@
 package com.example.demo;
 
 public class ThreadTest {
-	
-
-	static Integer x = 1;
-	
-	synchronized static void testThread (String test) {
-		try {
-			System.out.println(test);
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+	static boolean done1 = false;
+	static boolean done2 = false;
 
 	public static void main(String[] args) {
 		
+		Account account = new Account();
+
 
 		Thread thread1 = new Thread(() -> {
-			System.out.println(x);
-		});
-		Thread thread2 = new Thread(() -> {
-			x = 2;
-			System.out.println(x);
+			for(int i = 0; i < 1000000; i++) {
+				account.increase();
+				System.out.println(account.getCount());
+			}
+			done1 = true;
 
 		});
-		Thread thread3 = new Thread(() -> {
-			x = 3;
-			System.out.println(x);
+		Thread thread2 = new Thread(() -> {
+			for(int i = 0; i < 1000000; i++) {
+				account.deincrease();
+				System.out.println(account.getCount());
+			}
+			done2 = true;
 		});
-				
+
 		thread1.start();
 		thread2.start();
-		thread3.start();
+		
+		System.out.println(account.getCount());
+
+	}
+
+	static class Account {
+		private int count = 0;
+
+		public synchronized void increase() {
+			this.count++;
+			notify();
+		}
+
+		public synchronized void deincrease() {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(this.count < 0) {
+				this.count--;
+			}
+		}
+
+		public int getCount() {
+			return count;
+		}
+
 	}
 }
