8d0b69e8e4274ddde572111d9e86c4e2d3269f49
thai.dang <dangthai121200@gmail.com>
2023-08-26 01:49:30 +0700

    dockerfile and docker compose

--- WatchMovieGN-Movie/src/main/java/watch/movie/gn/WatchMovieGnBackendApplication.java
+++ WatchMovieGN-Movie/src/main/java/watch/movie/gn/WatchMovieGnBackendApplication.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableReactiveElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,7 +12,7 @@ import watch.movie.gn.foreignkey.MovieForeign;
 
 @EnableReactiveElasticsearchRepositories
 @EnableJpaRepositories
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class WatchMovieGnBackendApplication implements CommandLineRunner {
 
 	@Autowired
