22015b1b52b1686f22af69717bd0299f16b67e87
thai.dang <dangthai121200@gmail.com>
2023-10-16 12:39:15 +0700

    Add spring cloud

---
+++ WatchMovieGN-BE/WatchMovieGN-Cloud/src/test/java/watch/com/gn/WatchMovieGnCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package watch.com.gn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchMovieGnCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
