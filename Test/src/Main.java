b9c6f27220f2ef4c9a126075cba80109e72fa8fb
thai.dang <dangthai121200@gmail.com>
2024-06-29 11:42:52 +0700

    test

---
+++ Test/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		String a = "abc";
+		String b = new String("abc");
+		int c = 3;
+		Integer d = new Integer(3);
+		System.out.println(a.equals(b));
+	}
+
+}
